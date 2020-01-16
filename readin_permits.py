d90ada58e9e10f19eb5a75b9415d8e00712a147d
alxfed <alxfed@gmail.com>
2020-01-16 15:04:30 -0600

    initial commit

--- readin_permits.py
+++ readin_permits.py
@@ -9,25 +9,33 @@ import sorting
 
 
 def main():
-    dataset = 'ydr8-5enu'  # permits data
-    column = 'issue_date'
-    exclude_columns = ['location'] # 'xcoordinate, ycoordinate, latitude, longitude,
+    dataset = 'ydr8-5enu'  # permits dataset
+    # strings of parameters for request
     start_dt = dt.datetime(year=2019, month=12, day=1, hour=0, minute=0, second=0)
     start_str = start_dt.strftime('%Y-%m-%dT%H:%M:%S')
     end_dt = dt.datetime(year=2019, month=12, day=28, hour=0, minute=0, second=0)
     end_str = end_dt.strftime('%Y-%m-%dT%H:%M:%S')
+    # the column 'where' will be applied to
+    column = 'issue_date'
+    # socrata request(s)
     response = socradata.datasets.where_a_lot_between(dataset, column, start_str, end_str)
+
+    # exclude what can not fit into a database table
+    exclude_columns = ['location']  # 'xcoordinate, ycoordinate, latitude, longitude,
     result = pd.DataFrame.from_records(response, exclude=exclude_columns)
-    result['application_start_date'] = pd.to_datetime(result['application_start_date'], errors='coerce')
-    result['issue_date'] = pd.to_datetime(result['issue_date'], errors='coerce')
-    result['reported_cost'] = pd.to_numeric(result['reported_cost'], errors='coerce')
-    result['xcoordinate'] = pd.to_numeric(result['xcoordinate'])
-    result['ycoordinate'] = pd.to_numeric(result['ycoordinate'])
-    result['latitude'] = pd.to_numeric(result['latitude'])
-    result['longitude'] = pd.to_numeric(result['longitude'])
+    # NOT astype()
+    result['application_start_date']    = pd.to_datetime(result['application_start_date'], errors='coerce')
+    result['issue_date']                = pd.to_datetime(result['issue_date'], errors='coerce')
+    result['reported_cost']             = pd.to_numeric(result['reported_cost'], errors='coerce')
+    result['xcoordinate']               = pd.to_numeric(result['xcoordinate'])
+    result['ycoordinate']               = pd.to_numeric(result['ycoordinate'])
+    result['latitude']                  = pd.to_numeric(result['latitude'])
+    result['longitude']                 = pd.to_numeric(result['longitude'])
 
     conn = sqlalc.create_engine(sorting.HOME_DATABASE_URI)
-    result.to_sql(name=sorting.NEW_PERMITS_TABLE, con=conn, if_exists='replace', index=False)
+    result.to_sql(name=sorting.NEW_PERMITS_TABLE,
+                  con=conn, if_exists='replace',
+                  index=False)
     return
 
 
