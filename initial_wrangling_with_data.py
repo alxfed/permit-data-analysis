e703c31d3d427f11a425c63127c01bc85475076b
alxfed <alxfed@gmail.com>
2020-01-18 08:51:37 -0600

    wrangling with data

--- initial_wrangling_with_data.py
+++ initial_wrangling_with_data.py
@@ -12,18 +12,25 @@ def main():
 
     permits = pd.read_sql_table(table_name=sorting.PERMITS_TABLE, con=conn_source)
     # all types of permits
-    types = list(permits['permit_type'].unique())
+    types = list(permits['permit_type'].unique())  # unique returns a numpy Array
+    permit_types = pd.DataFrame(types, columns=['permit_type'])
+    permit_types.to_sql(name=sorting.constants.PERMIT_TYPES_TABLE,
+                        con=conn_target, if_exists='replace',
+                        index=False)
+    print('Saved permit types to ', sorting.constants.PERMIT_TYPES_TABLE, '\n\n')
 
-    # create separate auxiliary tables
+    # create/save separate auxiliary table for each type
     for type in types:
         permits_of_type = pd.DataFrame()
         permits_of_type = permits[permits['permit_type'] == type]
-        permits_of_type.info()
+        permits_of_type.info() # print out a summary of what will be saved
         permits_of_type.to_sql(name=type,
                                con=conn_target, if_exists='replace',
                                index=True)        # with indexes so that it can be reassembled
 
         print('Saved table ', type, '  with its index\n\n')
+    else:
+        print('Done saving to:  ', sorting.constants.TARGET_DATABASE_URI)
     # permits.head(n=2)
     return
 
