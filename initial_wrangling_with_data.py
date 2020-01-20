5d5f3e7cba667bb568fa291b9ce290b250ace313
alxfed <alxfed@gmail.com>
2020-01-20 09:53:12 -0600

    first wrangling with data

--- initial_wrangling_with_data.py
+++ initial_wrangling_with_data.py
@@ -4,6 +4,7 @@
 import sqlalchemy as sqlalc
 import pandas as pd
 import sorting
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -12,15 +13,24 @@ def main():
 
     permits = pd.read_sql_table(table_name=sorting.PERMITS_TABLE, con=conn_source)
     # all types of permits
-    types = list(permits['permit_type'].unique())  # unique returns a numpy Array
-    permit_types = pd.DataFrame(types, columns=['permit_type'])
+    types_list = list(permits['permit_type'].unique())  # unique returns a numpy Array
+    permit_types = pd.DataFrame(types_list, columns=['permit_type'])
     permit_types.to_sql(name=sorting.constants.PERMIT_TYPES_TABLE,
                         con=conn_target, if_exists='replace',
                         index=False)
     print('Saved permit types to ', sorting.constants.PERMIT_TYPES_TABLE, '\n\n')
 
+    # all_columns = permits.columns.values
+    useful_columns_for_plotting = ['id', 'permit_', 'permit_type', 'review_type',
+                                   'application_start_date', 'issue_date', 'processing_time',
+                                   'street_number', 'street_direction', 'street_name', 'suffix',
+                                   'work_description', 'reported_cost',
+                                   'xcoordinate', 'ycoordinate', 'latitude', 'longitude']
+
+    permits_for_plotting = permits.loc[:,useful_columns_for_plotting]  # this is a subsetted copy
+
     # create/save separate auxiliary table for each type
-    for type in types:
+    for type in types_list:
         permits_of_type = pd.DataFrame()
         permits_of_type = permits[permits['permit_type'] == type]
         permits_of_type.info() # print out a summary of what will be saved
